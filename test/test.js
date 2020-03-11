8b81de779fad806bc0150493280efbd7b6257e84
Aaron Huggins <aaron.huggins@gmail.com>
2020-03-11 13:59:36 -0500

    Added gitignore, started on tests

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const { PromiseWorker } = require('./index.js')
+
+const calculatePrimes = function calculatePrimes (max) {
+  return new PromiseWorker(function (workerData) {
+    const max = workerData
+    const store = []
+    const primes = []
+
+    for (let i = 2; i <= max; i++) {
+      if (!store[i]) {
+        primes.push(i)
+        for (let j = i << 1; j <= max; j += i) {
+          store[j] = true
+        }
+      }
+    }
+
+    return primes
+  }, { workerData: max })
+}
+
+describe('class PromiseWorker', () => {
+  it('should run sync function in thread', () => {
+    return calculatePrimes(12).then((result) => {
+      chai.expect(result).to.eql([2, 3, 5, 7, 11])
+    })
+  })
+
+  it('should run async function in thread', () => {
+    const msg = 'Darmok and Jalad at Tanagra.'
+    return new PromiseWorker((resolve, reject) => {
+      resolve(workerData)
+    }, { workerData: msg }).then((result) => {
+      chai.expect(result).to.eql(msg)
+    })
+  })
+})
